de7a2cbd01ddc67c99af313a838d23b6cbbe45ca
guidoasbun <gdrodrigo8@gmail.com>
2020-10-02 10:52:46 -0700

    barking dog method

--- src/BarkingDog.java
+++ src/BarkingDog.java
@@ -1,2 +1,20 @@
 public class BarkingDog {
+
+    public static void main(String[] args) {
+        boolean wakeup = shouldWakeUp(false, 2);
+        System.out.println(wakeup);
+    }
+
+    public static boolean shouldWakeUp(boolean barking, int hourOfDay){
+
+        boolean wakeUp = false;
+
+        if(hourOfDay <= 8 || hourOfDay >= 22){
+            wakeUp = false;
+        }else {
+            wakeUp = true;
+        }
+        return wakeUp;
+    }
+
 }
